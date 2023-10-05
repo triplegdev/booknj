bbdfa8a747d155220bac102fda1d1b74b4e625a5
Anthony Garcia <triplegdev@gmail.com>
2023-10-05 02:43:36 -0400

    Added SpotImages.url to group clause

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -626,7 +626,7 @@ router.get('/', queryValidators, async (req, res) => {
             [fn('AVG', col('Reviews.stars')), 'avgRating'],
             [literal('CASE WHEN "SpotImages"."preview" = true THEN "SpotImages"."url" ELSE null END'), 'previewImage']
         ],
-        group: ['Spot.id', 'SpotImages.preview'],
+        group: ['Spot.id', 'SpotImages.preview', 'SpotImages.url'],
         order: [['id']],
         limit: size,
         offset: size * (page - 1),
